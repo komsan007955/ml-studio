3cf5f29777b10306c1d2283da323bb16bb062ac7
komsan007955 <komsan@blendata.co>
2026-01-11 22:24:55 +0700

    add ui app

--- app/ml_studio/main.py
+++ app/ml_studio/main.py
@@ -1,13 +1,15 @@
 from flask import Flask, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/")
 def index():
     return jsonify({
-        "message": "Welcome to the Portal API",
+        "message": "Welcome to ML Studio",
         "status": "Online",
-        "service": "portal-api"
+        "service": "ml_studio"
     })
 
 @app.route("/api/hello")
@@ -15,5 +17,4 @@ def hello():
     return jsonify({"data": "Hello from ML Studio!"})
 
 if __name__ == "__main__":
-    # We use 5000 internally; Docker will map it to a different external port
     app.run(host="0.0.0.0", port=5000)
